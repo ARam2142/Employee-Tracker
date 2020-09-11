2ddf80629eb2cfe8b82590250605048b81fbe260
Aram2142 <andresaaramirez@aol.com>
2020-09-10 20:13:30 -0400

    tables are finally free of errors:)

--- schema.sql
+++ schema.sql
@@ -4,58 +4,28 @@ CREATE DATABASE employeetrack_db;
 USE employeetrack_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    name VARCHAR(30) NULL,
+    id INT NOT NULL IDENTITY,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
-    title VARCHAR(30) NULL,
-    salary DECIMAL NULL,
+    id INT NOT NULL IDENTITY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
+    id INT NOT NULL IDENTITY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY (id)
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES role(id)
 );
-
-
-employee:
-
---id - INT PRIMARY KEY
-
---first_name - VARCHAR(30) to hold employee first name
-
---last_name - VARCHAR(30) to hold employee last name
-
---role_id - INT to hold reference to role employee has
-
---manager_id - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
-
-
-department:
-
---id - INT PRIMARY KEY
-
---name - VARCHAR(30) to hold department name
-
-
-role:
-
-
---id - INT PRIMARY KEY
-
---title -  VARCHAR(30) to hold role title
-
---salary -  DECIMAL to hold role salary
-
---department_id -  INT to hold reference to department role belongs to
 
 
