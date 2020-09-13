23523f3ba7f22deff15c3fd37bc639487c0c11ac
Aram2142 <andresaaramirez@aol.com>
2020-09-13 00:25:18 -0400

    changed numbers

--- seed.sql
+++ seed.sql
@@ -8,17 +8,17 @@ INSERT INTO department (name)
 VALUES('Engineering');
 
 INSERT INTO role (title, salary, department_id)
-VALUES('Sales Associate', 50.000, 1);
+VALUES('Sales Associate', 50000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES('Sales Lead', 60.000, 1);
+VALUES('Sales Lead', 60000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES('Lawyer', 80.000, 2);
+VALUES('Lawyer', 80000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES('Accountant', 70.000, 3);
+VALUES('Accountant', 70000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES('Software Engineer', 90.000, 4);
+VALUES('Software Engineer', 90000, 4);
 INSERT INTO role (title, salary, department_id)
-VALUES('Lead Engineer', 120.000, 4);
+VALUES('Lead Engineer', 120000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES('Bob', 'Stanley', 2, null);
